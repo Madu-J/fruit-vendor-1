7764eb980b63efb76bdaa0b7b0a07319f719fe55
Madu-J <>
2023-11-04 15:45:12 +0000

    Create get_dispatched_data function to get data sring from list

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('fruit_vendor')
+
+
+def get_dispatched_data():
+    """
+    Get dispatched figure input from list
+    """
+    print("Calculate dispatched figure from the last market.")
+    print("Data should be ten numbers separated by slash.")
+    print("Example: 475/475/500/470/600/580/400/160/420/480\n")
+
+    info_str = input("Enter your data here:")
+    print(f"the data provided is {info_str}")
+
+
+get_dispatched_data()
