ed1a3f1057270d894156af86551cfe2948b04db7
Madu-J <>
2023-11-04 18:19:14 +0000

    Add validator to check exactly 10 values provided data

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+    ]   
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -18,11 +18,27 @@ def get_dispatched_data():
     Get dispatched figure input from list
     """
     print("Calculate dispatched figure from the last market.")
-    print("Data should be ten numbers separated by slash.")
-    print("Example: 475/475/500/470/600/580/400/160/420/480\n")
+    print("Data should be ten numbers separated by commas.")
+    print("Example: 475,475,500,470,600,580,400,160,420,480\n")
 
-    info_str = input("Enter your data here:")
-    print(f"the data provided is {info_str}")
+    data_str = input("Enter your data here: ")
+    dispatched_data = data_str.split(",")
+    validate_data(dispatched_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings can not be converted into int,
+    or if there aren't exactly 10 values.
+    """
+    try:
+        if len(values) != 10:
+            raise ValueError(
+                f"Exactly 10 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
 
 
 get_dispatched_data()
