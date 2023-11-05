34de6ef8508635d933f8967c094e974889865dcc
Madu-J <>
2023-11-05 14:29:58 +0000

    Add user loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -17,13 +17,20 @@ def get_dispatched_data():
     """
     Get dispatched figure input from list
     """
-    print("Calculate dispatched figure from the last market.")
-    print("Data should be ten numbers separated by commas.")
-    print("Example: 475,475,500,470,600,580,400,160,420,480\n")
+    while True:
+       print("Calculate dispatched from stock list.")
+       print("Data should be ten numbers separated by commas.")
+       print("Example: 475,475,500,470,600,580,400,160,420,480\n")
 
-    data_str = input("Enter your data here: ")
-    dispatched_data = data_str.split(",")
-    validate_data(dispatched_data)
+       data_str = input("Enter your data here: ")
+
+       dispatched_data = data_str.split(",")
+
+       if validate_data(dispatched_data):
+        print("Data is valid!")
+        break
+
+    return dispatched_data
 
 
 def validate_data(values):
@@ -32,13 +39,18 @@ def validate_data(values):
     Raises ValueError if strings can not be converted into int,
     or if there aren't exactly 10 values.
     """
+    print(values)
     try:
+        [int(value) for value in values]
         if len(values) != 10:
             raise ValueError(
                 f"Exactly 10 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 
-get_dispatched_data()
+data = get_dispatched_data()
